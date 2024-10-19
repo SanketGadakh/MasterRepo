cf39d06da48729fe658572b8d1ff4779879651f8
sanket <sanket@192.168.1.37>
2024-10-19 23:34:02 +0530

    new code change for git

--- Amazon/src/test/java/in/amazon/tests/SamleClass.java
+++ Amazon/src/test/java/in/amazon/tests/SamleClass.java
@@ -4,6 +4,10 @@ public class SamleClass {
 
 	public static void main(String[] args) {
 System.out.println("GitHub Practice");
+System.out.println("GitHub Practice");
+
+System.out.println("GitHub Practice");
+
 	}
 
 }
