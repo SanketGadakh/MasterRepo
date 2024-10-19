a81c0254ef4bdf7c636a41a95a88659c6920e94b
sanket <sanket@192.168.1.37>
2024-10-19 23:38:19 +0530

    Last commit

--- Amazon/src/test/java/in/amazon/tests/SamleClass.java
+++ Amazon/src/test/java/in/amazon/tests/SamleClass.java
@@ -4,9 +4,7 @@ public class SamleClass {
 
 	public static void main(String[] args) {
 System.out.println("GitHub Practice");
-System.out.println("GitHub Practice");
 
-System.out.println("GitHub Practice");
 
 	}
 
